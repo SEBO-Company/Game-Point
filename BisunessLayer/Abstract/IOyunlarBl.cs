9331b0f87953328adb57863b2aedcd58902af4d2
Atalay Berke Cin <berkecinofficial@gmail.com>
2023-12-26 02:00:56 +0300

    GamePoint_Backend
    Atalay Berke Cin
    205541052

---
+++ BisunessLayer/Abstract/IOyunlarBl.cs
@@ -0,0 +1,16 @@
+﻿using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BisunessLayer.Abstract
+{
+   public interface IOyunlarBl:IGenericBl<Oyunlar>
+    {
+        List<Oyunlar> TGetListwithCategoryPlatform();
+        List<Oyunlar> TGetListwithCategoryPlatformbyFilter(int id);
+        List<Oyunlar> TGetSearchListByFilter(string p);
+    }
+}
---
+++ DenemeGamePoint2/Controllers/TumOyunlarController.cs
@@ -0,0 +1,32 @@
+﻿using BisunessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using X.PagedList;
+namespace DenemeGamePoint2.Controllers
+{
+    public class TumOyunlarController : Controller
+    {
+
+        OyunlarBl oyunbl = new OyunlarBl(new EfOyunlarDal());
+       
+
+        public IActionResult Index(string p)
+        {
+            if (p == null)
+            {
+                var values2 = oyunbl.TGetListwithCategoryPlatform();
+                return PartialView(values2);
+            }
+            var values = oyunbl.TGetSearchListByFilter(p);
+            return PartialView(values);
+        }
+
+      
+
+    }
+}
